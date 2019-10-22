bd62086e0b04f1c93fdc1154b017247074de0d51
william.lima@audiogene.com.br <william.lima@audiogene.com.br>
2019-10-22 14:31:26 -0300

    edit

--- Licenca/Class1.cs
+++ Licenca/Class1.cs
@@ -5,5 +5,7 @@ namespace Licenca
     public class Class1
     {
         public string TRIAL { get; set; }
+        public string AVALIACAO { get; set; }
+        public string LIBERADO { get; set; }
     }
 }
